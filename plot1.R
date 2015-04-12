0ab9d60e700cf32781dc164ede83337082b34aca
wavygravy <wayvaw@gmail.com>
2015-04-12 19:26:37 -0400

    adding plot1.R file

---
+++ plot1.R
@@ -0,0 +1,5 @@
+URL <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(URL, destfile="./household_power_consumption.zip")
+unzip("household_power_consumption.zip")
+
+##laptop died and I didn't save my work! :(
